b8ea4c933f1421623c62ef0eb203dbde2cef781c
Jakub Urbanski <s23525@pjwstk.edu.pl>
2023-06-03 20:56:08 +0200

    Initialize project using Create React App

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Buttons from './routes/Buttons';
-import Divs from './routes/Divs';
-import Header from './fragments/Header';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-    return (
-        <Router>
-            <Header />
-            <Routes>
-                <Route path="/divs" element={<Divs />} />
-                <Route path="/buttons" element={<Buttons />} />
-            </Routes>
-        </Router>
-    );
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
